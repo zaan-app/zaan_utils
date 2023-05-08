9ee121f51ecc90550244740e4500a0a8252cf612
ameerfayiz <41417646+ameerfayiz@users.noreply.github.com>
2023-05-08 09:52:46 +0530

    added prettyJson

--- lib/src/public_utils.dart
+++ lib/src/public_utils.dart
@@ -1,5 +1,14 @@
+import 'dart:convert';
+
 class PublicUtils {
   static String cleanNumber(String garbageNumber) {
     return garbageNumber.replaceAll(RegExp(r'[^\d]'), '');
   }
+
+  String prettyJson(Map<String, dynamic> json, [bool withIndent = true]) {
+    JsonEncoder encoder = JsonEncoder.withIndent(withIndent ? '  ' : '');
+    String prettyPrintedJson = encoder.convert(json);
+    return prettyPrintedJson;
+  }
+
 }
